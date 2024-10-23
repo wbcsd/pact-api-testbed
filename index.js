f627d32946bec94218d12c3d9c676c9097ac7ed3
okada <>
2024-10-23 18:43:32 +0900

    fix: All configuration items are now supported in CLI mode. URL format restrictions have been relaxed. Some default values ​​have been changed.

--- index.js
+++ index.js
@@ -101,9 +101,15 @@ async function executeWithCli(versions) {
         },
         {
             name: "authContextPath",
-            message: "Authentication context path (e.g. https://example.com/auth)",
+            message: "Authentication context path\nTypically, the endpoint is this context path plus /auth/token. \ne.g. https://example.com/auth\n",
             type: "input", 
-            validate: answer => /^https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)$/.test(answer)
+            validate: answer => {
+                if(/^https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\b([-a-zA-Z0-9()@:%_\+.~#?&\/=]*)$/.test(answer)) {
+                    return true;
+                }else {
+                    return "Please enter a URL string.";
+                }
+            }
         },
         {
             name: "userName",
@@ -117,24 +123,33 @@ async function executeWithCli(versions) {
         },
         {
             name: "dataContextPath",
-            message: "Data context path (e.g. https://example.com/data)",
+            message: "Data context path\nIn Tech Spec ver2, the endpoint is this context path with /2/footprints appended to it.\ne.g. https://example.com/app\n",
             type: "input", 
-            validate: answer => /^https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)$/.test(answer)
+            validate: answer => {
+                if(/^https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\b([-a-zA-Z0-9()@:%_\+.~#?&\/=]*)$/.test(answer)) {
+                    return true;
+                }else {
+                    return "Please enter a URL string.";
+                }
+            }
         },
         {
             name: "filterSupport",
             message: "Does the system support the $filter request parameter in Action ListFootprints?",
-            type: "confirm"
+            type: "confirm",
+            default: true
         },
         {
             name: "limitSupport",
             message: "Does the system support the limit request parameter in Action ListFootprints?",
-            type: "confirm"
+            type: "confirm",
+            default: true
         },
         {
             name: "eventsSupport",
             message: "Does the system support Action Events?",
-            type: "confirm"
+            type: "confirm",
+            default: true
         },
         {
             name: "log",
@@ -145,15 +160,55 @@ async function executeWithCli(versions) {
         {
             name: "verboseLog",
             message: "Do you want detailed log output? (e.g. response content)",
-            type: "confirm"
+            type: "confirm",
+            default: false
         },
         {
             name: "stubContextPath",
-            message: "Enter the context path of the stub server.",
+            message: "Enter the context path of the stub server.\nThe stub server is required for testing asynchronous responses from the test app in Action Events.\nSet this URL to the test app as necessary.\n",
             type: "input",
             default: "http://localhost:3000"
+        },
+        {
+            name: "keepStub",
+            message: "Do you want to keep the stub server running?\nEnable this if you want to verify asynchronous responses to Action Events from your test target.",
+            type: "confirm",
+            default: true
+        },
+        {
+            name: "stubDataEnabled",
+            message: "Do you want to set the content of the data returned by the stub server?",
+            type: "confirm",
+            default: false
+        },
+        {
+            name: "stubData.companyIds",
+            message: "The company identifier included in the data when the stub server responds to Action GetFootprint/ListFootprints.\n",
+            type: "input",
+            when: answer => answer.stubDataEnabled
+        },
+        {
+            name: "stubData.productIds",
+            message: "The product identifier included in the data when the stub server responds to Action GetFootprint/ListFootprints.\n",
+            type: "input",
+            when: answer => answer.stubDataEnabled
         }
     ]);
+    
+    delete setting.stubDataEnabled;
+    if(setting.stubData != null) {
+        if(setting.stubData.companyIds != null && setting.stubData.companyIds.length > 0) {
+            setting.stubData.companyIds = [setting.stubData.companyIds];
+        }else {
+            delete setting.stubData.companyIds;
+        }
+        if(setting.stubData.productIds != null && setting.stubData.productIds.length > 0) {
+            setting.stubData.productIds = [setting.stubData.productIds];
+        }else {
+            delete setting.stubData.productIds;
+        }
+    }
+    
     try {
         await PathfinderValidator.validate(setting);
         if(setting.keepStub == undefined || !setting.keepStub) {
